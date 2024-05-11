f38abcbe10835825e480640f8efbf60023499075
byl0561 <byl0561@gmail.com>
2024-05-12 01:06:53 +0800

    bugfix

--- movie/views.py
+++ movie/views.py
@@ -17,10 +17,20 @@ def diff_douban_250(request):
     local_movie = crawl_local()
     missing_movies = get_missing_movies(douban_250_movie, local_movie)
     extra_movies = get_extra_movies(douban_250_movie, local_movie)
+    retained_extra_movie_set_names = set([extra_movie.get_collection_name() for extra_movie in extra_movies
+                                          if is_retained(extra_movie)])
+
     return JsonResponse({
         'missing_movies': [missing_movie.to_dict() for missing_movie in missing_movies],
-        'extra_movies': [extra_movie.to_dict() for extra_movie in extra_movies]
+        'extra_movies': [extra_movie.to_dict() for extra_movie in extra_movies
+                         if not is_retained(extra_movie) and
+                         (extra_movie.get_collection_name() is None
+                          or extra_movie.get_collection_name() not in retained_extra_movie_set_names)],
     })
+
+
+def is_retained(movie: Movie) -> bool:
+    return movie.get_rate().score > 7.5 and movie.get_rate().votes > 500
 
 
 def complete_local_movie_collection(request):
@@ -37,13 +47,18 @@ def complete_local_movie_collection(request):
 
     resp_dict = {}
     for tmdb_set_id, tmdb_ids in existing_movie_sets.items():
-        time.sleep(0.5)
+        time.sleep(0.2)
         tmdb_movies_in_set = get_movies_in_set(tmdb_set_id)
         missing_movies = []
         for tmdb_movie in tmdb_movies_in_set:
-            if tmdb_movie.id not in tmdb_ids and 0 < tmdb_movie.year <= int(datetime.now().year):
+            if tmdb_movie.id not in tmdb_ids and legal_movie(tmdb_movie):
                 missing_movies.append(tmdb_movie)
         if len(missing_movies) > 0:
             resp_dict[missing_movies[0].move_set.name] = [missing_movie.to_dict() for missing_movie in missing_movies]
 
     return JsonResponse(resp_dict)
+
+
+def legal_movie(movie: Movie) -> bool:
+    return (0 < movie.get_year() < int(datetime.now().year) or
+            (movie.get_year() == int(datetime.now().year) and movie.get_rate().score > 0))
